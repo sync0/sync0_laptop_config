285b8d2d433112d8bdbad8297978a8d08fb25657
sync0 <cantorlunae@gmail.com>
2017-12-04 23:59:55 +0100

    untrack some bothersome files

--- .emacs.d/elpa/counsel-20170906.1103/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20170906.1103" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "7284ca61fddc82b2716cc7c02c261efe02e109b7" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
