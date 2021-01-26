5844c2beece0564329761dfec97980e6da6246ed
sync0 <carc.sync0@gmail.com>
2021-01-26 00:15:53 -0500

    correction to the code to prepare migration

---
+++ .emacs.d/custom_settings.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-auto-commit nil nil nil "Customized with use-package company")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(git-gutter:added ((t (:background "#7ccd7c"))))
+ '(git-gutter:deleted ((t (:background "ee6363"))))
+ '(git-gutter:modified ((t (:background "#3a81c3")))))
